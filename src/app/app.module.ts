a9f7292f1fd7c8e7565bb165f32cb6a1e003a4b9
ejelome <ejelome@gmail.com>
2022-05-24 21:48:27 +0200

    Add diff component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiffComponent } from './diff/diff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiffComponent
   ],
   imports: [
     BrowserModule,
